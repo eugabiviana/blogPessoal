8953c95851ef5ef4e2ec5a74a238c6993c59a7f1
eugabiviana <qqcoisa@g>
2020-11-05 15:27:27 -0300

    Component cadastrar and login

--- blogPessoal/src/app/cadastro/cadastro.component.ts
+++ blogPessoal/src/app/cadastro/cadastro.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from '../model/User';
+import { AuthService } from '../service/auth.service';
 
 @Component({
   selector: 'app-cadastro',
@@ -7,9 +10,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastroComponent implements OnInit {
 
-  constructor() { }
+  user: User = new User()
+  senha: string
 
-  ngOnInit(): void {
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) { }
+
+  ngOnInit(){
+  }
+
+  conferirSenha(event: any){
+    this.senha = event.target.value 
+  }
+
+  cadastrar(){
+    if (this.senha === this.user.senha){
+      this.authService.cadastrar(this.user).subscribe((resp:User) =>{
+        this.user = resp
+        this.router.navigate(['/login'])
+        alert('Usuário cadastrado com sucesso!')
+      })
+    } else {
+      alert('Suas senhas não conferem!')
+    }    
   }
 
 }
--- blogPessoal/src/app/service/auth.service.ts
+++ blogPessoal/src/app/service/auth.service.ts
@@ -11,10 +11,32 @@ export class AuthService {
   constructor(private http: HttpClient) { }
 
   logar(userLogin: UserLogin) {
-    return this.http.post('http://localhost:9000/usuarios/logar', userLogin)
+    return this.http.post('http://localhost:8080/usuarios/logar', userLogin)
   }
 
   cadastrar(user: User){
-    return this.http.post('http://localhost:9000/usuarios/cadastrar', user)
+    return this.http.post('http://localhost:8080/usuarios/cadastrar', user)
+  }
+
+  btnSair(){
+    let ok = false
+    let token = localStorage.getItem('token')
+
+    if(token != null){
+      ok = true
+    }
+
+    return ok 
+  }
+
+  btnLogin(){
+    let ok = false
+    let token = localStorage.getItem('token')
+
+    if(token == null){
+      ok = true
+    }
+
+    return ok 
   }
 }
